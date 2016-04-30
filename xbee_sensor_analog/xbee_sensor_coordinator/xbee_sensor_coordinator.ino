d242d5588632860003b8f1c9672787f52eaf941f
Paras Shah <parasshah25@yahoo.co.in>
2016-04-30 10:35:23 +0530

    Added files via upload

---
+++ xbee_sensor_analog/xbee_sensor_coordinator/xbee_sensor_coordinator.ino
@@ -0,0 +1,29 @@
+float ldr;
+void setup() 
+{
+  Serial.begin(9600);
+}
+void loop() 
+{
+if (Serial.available() >21)
+  {
+   if (Serial.read() == 0x7E)
+    {
+     for (int i=0; i<19; i++)
+      {
+       byte discardByte = Serial.read();
+      }
+      int analogMSB = Serial.read();
+      int analogLSB = Serial.read();
+      int analogReading = analogLSB + (analogMSB * 256);
+      ldr = analogReading * 1023.0 * 1.23;
+      ldr = ldr - 0.5;
+      ldr = ldr * 0.01;
+      ldr = ldr * 9/5 + 32;
+      unsigned long int data = ldr;
+      Serial.print(data);
+      Serial.println(" light value ");
+      delay(200);
+    }
+  }
+}
---
+++ xbee_sensor_digital/xbee_sensor_coordinator/xbee_sensor_coordinator.ino
@@ -0,0 +1,28 @@
+int readValue =0;
+void setup() 
+{
+  Serial.begin(9600);
+}
+void loop() 
+{
+if (Serial.available() >21)
+  {
+   if (Serial.read() == 0x7E)
+    {
+     for (int i=0; i<19; i++)
+      {
+       byte discard = Serial.read();
+      }
+      readValue = Serial.read();
+      Serial.print("Paras Shah: ");
+      if (readValue == 0)
+       {
+        Serial.println("No Touch");
+       }
+        else if (readValue == 16)
+          {
+            Serial.println("Touch");
+          }
+       }
+   }
+}
